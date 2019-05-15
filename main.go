fd6c18e5558796be46021c5de23c056a3b11492a
sapphi-red <green@sapphi.red>
2019-05-15 17:11:04 +0900

    街の情報を追加するAPI

--- main.go
+++ main.go
@@ -16,25 +16,30 @@ type Country struct {
 	Name           string  `json:"name,omitempty"  db:"Name"`
 	Continent      string  `json:"continent,omitempty"  db:"Continent"`
 	Region         string  `json:"region,omitempty"  db:"Region"`
-	SurfaceArea    float64 `json:"surfaceArea,omitempty"  db:"SurfaceArea"`
-	IndepYear      int     `json:"indepYear,omitempty"  db:"IndepYear"`
+	SurfaceArea    float64 `json:"surface_area,omitempty"  db:"SurfaceArea"`
+	IndepYear      int     `json:"indep_year,omitempty"  db:"IndepYear"`
 	Population     int     `json:"population,omitempty"  db:"Population"`
-	LifeExpectancy float64 `json:"lifeExpectancy,omitempty"  db:"LifeExpectancy"`
-	GNP            float64 `json:"gnp,omitempty"  db:"GNP"`
-	GNPOld         float64 `json:"gnpOld,omitempty"  db:"GNPOld"`
-	LocalName      string  `json:"localName,omitempty"  db:"LocalName"`
-	GovernmentForm string  `json:"governmentForm,omitempty"  db:"GovernmentForm"`
-	HeadOfState    string  `json:"headOfState,omitempty"  db:"HeadOfState"`
+	LifeExpectancy float64 `json:"life_expectancy,omitempty"  db:"LifeExpectancy"`
+	GNP            float64 `json:"GNP,omitempty"  db:"GNP"`
+	GNPOld         float64 `json:"GNP_old,omitempty"  db:"GNPOld"`
+	LocalName      string  `json:"local_name,omitempty"  db:"LocalName"`
+	GovernmentForm string  `json:"government_form,omitempty"  db:"GovernmentForm"`
+	HeadOfState    string  `json:"head_of_state,omitempty"  db:"HeadOfState"`
 	Capital       int     `json:"capital,omitempty"  db:"Capital"`
 	Code2          string  `json:"code2,omitempty"  db:"Code2"`
 }
 
 type City struct {
-	ID          int    `json:"id,omitempty" db:"ID"`
+	ID          int    `json:"ID,omitempty" db:"ID"`
 	Name        string `json:"name,omitempty" db:"Name"`
-	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	CountryCode string `json:"country_code,omitempty" db:"CountryCode"`
 	District    string `json:"district,omitempty" db:"District"`
 	Population  int    `json:"population,omitempty"  db:"Population"`
+}
+
+type ResponseData struct {
+	Type  string `json:"type"`
+	Content string `json:"content"`
 }
 
 var (
@@ -62,6 +67,8 @@ func main() {
 	})
 	e.GET("/cities/:cityName", getCityInfoHandler)
 
+	e.POST("/cities", postCityInfoHandler)
+
 	e.Start(":12100")
 }
 
@@ -72,7 +79,7 @@ func getCityInfoHandler(c echo.Context) error {
 	city := City{}
 	db.Get(
 		&city,
-		"SELECT * FROM city WHERE Name = ?",
+		`SELECT * FROM city WHERE Name = ?`,
 		cityName,
 	)
 	if city.Name == "" {
@@ -80,4 +87,33 @@ func getCityInfoHandler(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, city)
+}
+
+func postCityInfoHandler(c echo.Context) error {
+	data := new(City)
+	err := c.Bind(data)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ResponseData{
+			Type: "Error",
+			Content: "Parse Error",
+		})
+	}
+
+	_, err = db.Exec(
+		`INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)`,
+		data.Name,
+		data.CountryCode,
+		data.District,
+		data.Population,
+	)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ResponseData{
+			Type: "Error",
+			Content: fmt.Sprintf("%v", err), //"SQL Error",
+		})
+	}
+	return c.JSON(http.StatusOK, ResponseData{
+		Type: "Success",
+		Content: "Success",
+	})
 }
