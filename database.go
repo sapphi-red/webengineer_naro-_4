bfb62142113a35d97273466d0e93460dff09a9b9
sapphi-red <green@sapphi.red>
2019-05-15 18:44:17 +0900

    データベース関連を分離

---
+++ database.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"os"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+type Country struct {
+	Code           string  `json:"code,omitempty"  db:"Code"`
+	Name           string  `json:"name,omitempty"  db:"Name"`
+	Continent      string  `json:"continent,omitempty"  db:"Continent"`
+	Region         string  `json:"region,omitempty"  db:"Region"`
+	SurfaceArea    float64 `json:"surface_area,omitempty"  db:"SurfaceArea"`
+	IndepYear      int     `json:"indep_year,omitempty"  db:"IndepYear"`
+	Population     int     `json:"population,omitempty"  db:"Population"`
+	LifeExpectancy float64 `json:"life_expectancy,omitempty"  db:"LifeExpectancy"`
+	GNP            float64 `json:"GNP,omitempty"  db:"GNP"`
+	GNPOld         float64 `json:"GNP_old,omitempty"  db:"GNPOld"`
+	LocalName      string  `json:"local_name,omitempty"  db:"LocalName"`
+	GovernmentForm string  `json:"government_form,omitempty"  db:"GovernmentForm"`
+	HeadOfState    string  `json:"head_of_state,omitempty"  db:"HeadOfState"`
+	Capital        int     `json:"capital,omitempty"  db:"Capital"`
+	Code2          string  `json:"code2,omitempty"  db:"Code2"`
+}
+
+type City struct {
+	ID          int    `json:"ID,omitempty" db:"ID"`
+	Name        string `json:"name,omitempty" db:"Name"`
+	CountryCode string `json:"country_code,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty"  db:"Population"`
+}
+
+func connectDB() *sqlx.DB {
+	db, err := sqlx.Connect("mysql", fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOSTNAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_DATABASE"),
+	))
+	if err != nil {
+		log.Fatalf("Cannot Connect to Database: %s", err)
+	}
+	return db
+}
+
+func getCity(name string) City {
+	city := City{}
+	db.Get(
+		&city,
+		`SELECT * FROM city WHERE Name = ?`,
+		name,
+	)
+	return city
+}
+
+func addCity(city *City) error {
+	_, err := db.Exec(
+		`INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)`,
+		city.Name,
+		city.CountryCode,
+		city.District,
+		city.Population,
+	)
+	return err
+}
+
+func deleteCity(name string) error {
+	_, err := db.Exec(
+		`DELETE FROM city WHERE Name = ?`,
+		name,
+	)
+	return err;
+}
--- main.go
+++ main.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/middleware"
-	"log"
 	"net/http"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
-
-type Country struct {
-	Code           string  `json:"code,omitempty"  db:"Code"`
-	Name           string  `json:"name,omitempty"  db:"Name"`
-	Continent      string  `json:"continent,omitempty"  db:"Continent"`
-	Region         string  `json:"region,omitempty"  db:"Region"`
-	SurfaceArea    float64 `json:"surface_area,omitempty"  db:"SurfaceArea"`
-	IndepYear      int     `json:"indep_year,omitempty"  db:"IndepYear"`
-	Population     int     `json:"population,omitempty"  db:"Population"`
-	LifeExpectancy float64 `json:"life_expectancy,omitempty"  db:"LifeExpectancy"`
-	GNP            float64 `json:"GNP,omitempty"  db:"GNP"`
-	GNPOld         float64 `json:"GNP_old,omitempty"  db:"GNPOld"`
-	LocalName      string  `json:"local_name,omitempty"  db:"LocalName"`
-	GovernmentForm string  `json:"government_form,omitempty"  db:"GovernmentForm"`
-	HeadOfState    string  `json:"head_of_state,omitempty"  db:"HeadOfState"`
-	Capital        int     `json:"capital,omitempty"  db:"Capital"`
-	Code2          string  `json:"code2,omitempty"  db:"Code2"`
-}
-
-type City struct {
-	ID          int    `json:"ID,omitempty" db:"ID"`
-	Name        string `json:"name,omitempty" db:"Name"`
-	CountryCode string `json:"country_code,omitempty" db:"CountryCode"`
-	District    string `json:"district,omitempty" db:"District"`
-	Population  int    `json:"population,omitempty"  db:"Population"`
-}
 
 type ResponseData struct {
 	Type    string `json:"type"`
@@ -49,19 +20,7 @@ var (
 )
 
 func main() {
-	_db, err := sqlx.Connect("mysql", fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOSTNAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_DATABASE"),
-	))
-	if err != nil {
-		log.Fatalf("Cannot Connect to Database: %s", err)
-	}
-	db = _db
-
+	db = connectDB()
 	store := createSessionStore()
 
 	e := echo.New()
@@ -91,12 +50,7 @@ func main() {
 func getCityInfoHandler(c echo.Context) error {
 	cityName := c.Param("cityName")
 
-	city := City{}
-	db.Get(
-		&city,
-		`SELECT * FROM city WHERE Name = ?`,
-		cityName,
-	)
+	city := getCity(cityName)
 	if city.Name == "" {
 		return c.NoContent(http.StatusNotFound)
 	}
@@ -114,13 +68,7 @@ func postCityInfoHandler(c echo.Context) error {
 		})
 	}
 
-	_, err = db.Exec(
-		`INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)`,
-		data.Name,
-		data.CountryCode,
-		data.District,
-		data.Population,
-	)
+	err = addCity(data)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, ResponseData{
 			Type:    "Error",
@@ -136,10 +84,7 @@ func postCityInfoHandler(c echo.Context) error {
 func deleteCityInfoHandler(c echo.Context) error {
 	cityName := c.Param("cityName")
 
-	_, err := db.Exec(
-		`DELETE FROM city WHERE Name = ?`,
-		cityName,
-	)
+	err := deleteCity(cityName)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, ResponseData{
 			Type:    "Error",
