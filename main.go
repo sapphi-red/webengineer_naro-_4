5a01fa5e4f5d9f9e8dc3711b3a893c8267337222
sapphi-red <green@sapphi.red>
2019-05-15 16:07:51 +0900

    WIP

--- main.go
+++ main.go
@@ -9,6 +9,24 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+type Country struct {
+	Code           string  `json:"code,omitempty"  db:"Code"`
+	Name           string  `json:"name,omitempty"  db:"Name"`
+	Continent      string  `json:"continent,omitempty"  db:"Continent"`
+	Region         string  `json:"region,omitempty"  db:"Region"`
+	SurfaceArea    float64 `json:"surfaceArea,omitempty"  db:"SurfaceArea"`
+	IndepYear      int     `json:"indepYear,omitempty"  db:"IndepYear"`
+	Population     int     `json:"population,omitempty"  db:"Population"`
+	LifeExpectancy float64 `json:"lifeExpectancy,omitempty"  db:"LifeExpectancy"`
+	GNP            float64 `json:"gnp,omitempty"  db:"GNP"`
+	GNPOld         float64 `json:"gnpOld,omitempty"  db:"GNPOld"`
+	LocalName      string  `json:"localName,omitempty"  db:"LocalName"`
+	GovernmentForm string  `json:"governmentForm,omitempty"  db:"GovernmentForm"`
+	HeadOfState    string  `json:"headOfState,omitempty"  db:"HeadOfState"`
+	Captital       int     `json:"captital,omitempty"  db:"Captital"`
+	Code2          string  `json:"code2,omitempty"  db:"Code2"`
+}
+
 type City struct {
 	ID          int    `json:"id,omitempty" db:"ID"`
 	Name        string `json:"name,omitempty" db:"Name"`
@@ -33,8 +51,19 @@ func main() {
 
 	cityName := os.Args[1]
 	city := City{}
-
 	db.Get(&city, fmt.Sprintf("SELECT * FROM city WHERE Name='%s'", cityName))
 
-	fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
+	country := Country{}
+	db.Get(&country, fmt.Sprintf("SELECT * FROM country WHERE Code='%s'", city.CountryCode))
+
+	fmt.Printf("%v\n", country)
+	/*
+	fmt.Printf(
+		"%sの人口は%s全体の%d%%\n",
+		cityName,
+		country.Name,
+		city.Population/country.Population*100,
+	)
+	*/
+
 }
